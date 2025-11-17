f8c1807247f902689973ca180b82909b32478a14
Julien Baouz <baouzjulien@gmail.com>
2025-11-17 20:29:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const markdownToHtmlTable = {
+  heading1: /^#\s+(.*)/,
+  heading2: /^##\s+(.*)/,
+  heading3: /^###\s+(.*)/,
+  heading4: /^####\s+(.*)/,
+  heading5: /^#####\s+(.*)/,
+  heading6: /^######\s+(.*)/,
 
+  bold: /(\*\*|__)(.*?)\1/g,
+  italic: /(\*|_)([^*_]+?)\1/g,
+
+  img: /!\[(.*?)\]\((.*?)\)/g,
+  link: /\[(.*?)\]\((.*?)\)/g,
+
+  quote: /^>\s+(.*)/i
+}
+
+const convertMarkdown = () => {
+
+};
